c377425b24bf07899ad92673450603851ecc97e7
abishek dulal <abishek.dulal666s@gmail.com>
2019-08-11 07:33:35 +0545

    first commit

---
+++ src/Component/LoginForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LoginForm() {
+
+    return (
+        <div>
+            <input> </input>
+            <input> </input>
+        </div>
+    )
+}
+
+export default LoginForm;
